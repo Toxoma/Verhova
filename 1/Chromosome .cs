dbe86e222dde85a32b71610ecc89f769b98a9ebd
D.Veselov <d.veselov@techmoney.ru>
2023-03-15 18:44:12 +0300

    Chromosome

---
+++ 1/Chromosome .cs
@@ -0,0 +1,18 @@
+﻿public class Chromosome
+{
+    public List<Gen> Gens {get; private set;} = new List<Gen>();
+
+    public Chromosome(List<Gen> gens){
+        this.Gens = gens;
+    }
+
+    public string GetExons(){
+        string result = "";
+        foreach (var item in this.Gens)
+        {
+            result+=item.Value;
+        }
+        return result;
+    }
+
+}
--- 1/Program.cs
+++ 1/Program.cs
@@ -1,4 +1,6 @@
-﻿Gen gen = new Gen(new List<bool>(){true,false,true});
-Console.WriteLine(gen.Value);
-gen.Mutation();
-Console.WriteLine(gen.Value);
+﻿Gen gen1 = new Gen(new List<bool>(){true,false,true});
+Gen gen2 = new Gen(new List<bool>(){false,false,true});
+Chromosome chromosome1 = new Chromosome(new List<Gen>(){gen1, gen2});
+
+
+Console.WriteLine(chromosome1.GetExons());
